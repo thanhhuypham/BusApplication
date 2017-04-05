f5604d84fae9fb71dac48fbdc8db971acde904bf
thanhhuypham <thanhhuy.25101996@gmail.com>
2017-04-05 22:46:14 +0700

    Update Main activity

--- app/src/main/java/com/example/administrator/busapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/busapp/MainActivity.java
@@ -1,32 +1,28 @@
 package com.example.administrator.busapp;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
+import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     BusAdapter adapter;
     private static ArrayList<Bus> arrList;
-    private EditText edtInput;
+    private EditText edtInputSearch;
     private Button btnTim;
     private ListView lvBus;
 
@@ -35,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         arrList = new ArrayList<Bus>();
-        edtInput = (EditText) findViewById(R.id.editTextSearch);
+        edtInputSearch = (EditText) findViewById(R.id.editTextSearch);
         btnTim = (Button) findViewById(R.id.btnTim);
         lvBus = (ListView) findViewById(R.id.listViewBus);
 
@@ -51,18 +47,18 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject items = jsonArray.getJSONObject(i);
 
                 String id = items.getString("bus_id").toString();
-                String name = items.getString("bus_name").toString();
+                final String name = items.getString("bus_name").toString();
 
                 Bus bus = new Bus(id, name);
                 arrList.add(bus);
 
                 adapter = new BusAdapter(MainActivity.this, R.layout.layout_item_listview, arrList);
                 lvBus.setAdapter(adapter);
+
             }
         }
         catch (JSONException e) {
             e.printStackTrace();
         }
     }
-
 }
