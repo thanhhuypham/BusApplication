d9ea68965d8ae1dedc1deb882dd483d033b30595
thanhhuypham <thanhhuy.25101996@gmail.com>
2017-04-05 19:40:56 +0700

    Add data json bus app

--- app/src/main/java/com/example/administrator/busapp/DataBusJSON.java
+++ app/src/main/java/com/example/administrator/busapp/DataBusJSON.java
@@ -34,6 +34,58 @@ public class DataBusJSON {
                     "   {\n" +
                     "      \"bus_id\":7,\n" +
                     "      \"bus_name\":\"Bến xe Chợ Lớn- Gò vấp\"\n" +
-                    "   }\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":8,\n" +
+                    "      \"bus_name\":\"Bến xe Quận 8- Đại học Quốc Gia\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":9,\n" +
+                    "      \"bus_name\":\"Chợ Lớn - Bình Chánh - Hưng Long\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":10,\n" +
+                    "      \"bus_name\":\"Đại học Quốc Gia- Bến xe Miền Tây\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":11,\n" +
+                    "      \"bus_name\":\"Bến Thành- Đầm Sen\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":12,\n" +
+                    "      \"bus_name\":\"Bến Thành - Thác Giang Điền\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":13,\n" +
+                    "      \"bus_name\":\"Công Viên 23/9 - Bến xe Củ Chi\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":14,\n" +
+                    "      \"bus_name\":\"Miền Đông- 3 tháng 2- Miền Tây\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":15,\n" +
+                    "      \"bus_name\":\"Bến Phú Định- Đầm Sen\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":16,\n" +
+                    "      \"bus_name\":\"Bến xe Chợ Lớn - Bến xe Tân Phú\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":17,\n" +
+                    "      \"bus_name\":\"Bến Xe Chợ Lớn - ĐH Sài Gòn - KCX Tân Thuận\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":18,\n" +
+                    "      \"bus_name\":\"Bến Thành - Chợ Hiệp Thành\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":19,\n" +
+                    "      \"bus_name\":\"Bến Thành - Khu Chế Xuất Linh Trung - Đại Học Quốc Gia\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":20,\n" +
+                    "      \"bus_name\":\"Bến Thành - Nhà Bè\"\n" +
+                    "   },\n" +
                     "]}";
 }
