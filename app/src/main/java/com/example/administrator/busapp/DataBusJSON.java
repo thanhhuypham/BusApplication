590ff52141734051b87ecbc98e3a7e8bec3da8c6
thanhhuypham <thanhhuy.25101996@gmail.com>
2017-04-05 19:32:15 +0700

    Create JSON Bus

---
+++ app/src/main/java/com/example/administrator/busapp/DataBusJSON.java
@@ -0,0 +1,39 @@
+package com.example.administrator.busapp;
+
+/**
+ * Created by Administrator on 05/04/2017.
+ */
+
+public class DataBusJSON {
+    public static String strJSON =
+            "{\"Bus\":[\n" +
+                    "   {\n" +
+                    "      \"bus_id\":1,\n" +
+                    "      \"bus_name\":\"Bến Thành - BX Chợ Lớn\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":2,\n" +
+                    "      \"bus_name\":\"Bến Thành - BX Miền Tây\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":3,\n" +
+                    "      \"bus_name\":\"Bến Thành - Thạnh Lộc\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":4,\n" +
+                    "      \"bus_name\":\"Bến Thành - Cộng Hòa - An Sương\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":5,\n" +
+                    "      \"bus_name\":\"Bến xe Chợ Lớn - Biên Hòa\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":6,\n" +
+                    "      \"bus_name\":\"Bến xe Chợ Lớn- Đại học Nông Lâm\"\n" +
+                    "   },\n" +
+                    "   {\n" +
+                    "      \"bus_id\":7,\n" +
+                    "      \"bus_name\":\"Bến xe Chợ Lớn- Gò vấp\"\n" +
+                    "   }\n" +
+                    "]}";
+}
